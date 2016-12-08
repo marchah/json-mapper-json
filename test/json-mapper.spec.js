dc1850370b9e97702e138f47ef06021086a64299
Marchah <marchah.dev@gmail.com>
2016-12-08 12:23:51 -0800

    test(item): add unit test, #10

--- test/json-mapper.spec.js
+++ test/json-mapper.spec.js
@@ -1185,6 +1185,36 @@ function unitTestForJsonMapper(fct) {
       done();
     });
   });
+  it('complex test with key word `$item`', (done) => {
+    fct({  
+      hits: {
+        total: 1,
+        hits: [{
+          _index: 'some_index',
+          _type: 'some_type',
+          _id: '123456',
+          _score: 1,
+          _source: {
+            id: 123456
+          },
+        }],
+      },
+    }, {
+      hits: {
+        path: 'hits.hits',
+        nested: {
+          id: '_source.id',
+          type: {
+            path: '$item',
+            formatting: (value) => (`${value._index}/${value._type}`),
+          },
+        },
+      },
+    }).then((result) => {
+      expect(result).to.eql({ hits: [{ id: 123456, type: 'some_index/some_type' }] });
+      done();
+    });
+  });
 }
 
 describe('jsonMapper', () => {
