938ba284aad6c90985743080e92d03db242324d7
marchah <marchah.dev@gmail.com>
2021-08-17 16:10:00 -0700

    test: add unit test case for issue #23

--- test/index.js
+++ test/index.js
@@ -1,6 +1,25 @@
+const expect = require('chai').expect;
+
+const jsonMapper = require('../lib/json-mapper.js');
+
 
 describe('Unit Testing', () => {
   require('./old.spec.js');
   require('./json-mapper.spec.js');
   require('./inttra.spec.js');
+
+
+  it('should test issue #23 question', async () => {
+    const res = await jsonMapper({ email: "a@example.com" },
+      {
+        "emails": {
+          path: "email",
+          formatting: (email) => {
+              return [{ email }];
+          },
+      },
+      });
+
+      expect(res).to.eql({ emails: [ { email: "a@example.com" }] });
+  });
 });
